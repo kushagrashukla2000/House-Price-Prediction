3625783e60b68011ad00471de40fa6e16522add4
Kushagra Shukla <kushagrashuklabp@gmail.com>
2026-02-01 20:09:05 +0530

    Heatmap plot and OOPs implementation

---
+++ heatmap.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from housePricePrediction import HousePrice
+
+class HeatMap(HousePrice):
+    def __init__(self) -> None:
+        super().__init__()
+        self.numerical_dataset = self.dataset.select_dtypes(include=['int64', 'float64'])
+
+    def plot(self):    
+        plt.figure(figsize=(12, 6))
+        sns.heatmap(self.numerical_dataset.corr(),
+            cmap='BrBG',
+            fmt='.2f',
+            linewidths=2,
+            annot=True)
+        plt.title("Correlation Heatmap of Numerical Features")
+        plt.tight_layout()
+        plt.savefig("HeatmapOutput/correlation_heatmap.png")
+        print("Heatmap saved as correlation_heatmap.png")
--- housePricePrediction.py
+++ housePricePrediction.py
@@ -2,6 +2,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-dataset = pd.read_excel("data/HousePricePrediction.xlsx")
+class HousePrice():
+    def __init__(self) -> None:
+        self.dataset = pd.read_excel("data/HousePricePrediction.xlsx")
+        self.categorical_cols = self.dataset.select_dtypes(include=['object']).columns
+        self.integer_cols = self.dataset.select_dtypes(include=['int64']).columns
+        self.float_cols = self.dataset.select_dtypes(include=['float64']).columns
 
-print(dataset.head(5))
+    def __str__(self) -> str:
+        return (
+            "Categorical variables:" + str(len(self.categorical_cols)) + "\n"
+            "Integer variables:" + str(len(self.integer_cols)) + "\n"
+            "Float variables:" + str(len(self.float64)) + "\n"
+        )
